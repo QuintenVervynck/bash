5d8623fea18384041e74597ff4b2f57ee715504b
QuintenVervynck <quinten.vervynck@gmail.com>
2020-10-23 16:03:00 +0200

    todo added

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# scripts_linux
+### Scripts_linux
+
 The scripts I use in linux (ubuntu)
+
+## todo
+
+# Description
+
+script to handle todo lists
+
+# Installation 
+
+run setup.bash in the /todo directory
+
+watch out, this will also make aan alias "todo" 
+if you already have this and you run setup.bash, you will have this alias twice in you ~/.bashrc file
---
+++ todo/setup.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir ~/.data 2> /dev/null
+mkdir ~/.data/todo
+echo '# todo : script from "scripts_linux" repository' >> ~/.bashrc
+echo 'alias todo="bash '$(pwd)'/todo.bash"' >> ~/.bashrc
+
+#!/bin/bash
+echo "do you want to delete this setup file for the todo script? [y/n]"
+read answer
+if  [[ $answer == "y" ]]; then
+        rm test.bash
+        echo "removed"
+else
+        echo "not removed"
+fi
+
