ff5a4f0e66b7d68ecef088c2ddc64674515bacc0
quintenvervynck <vervynck.quinten@gmail.com>
2020-10-25 10:02:11 +0100

    namefix

--- clirail/setup.bash
+++ clirail/setup.bash
@@ -5,7 +5,7 @@ sudo pip3 install clirail
 echo '# trein : script from "scripts_linux" repository, but its just an alias for a python-module called clirail' >> ~/.bashrc
 echo 'alias trein="clirail"' >> ~/.bashrc
 
-echo "do you want to delete this setup file for the todo script? [y/n]"
+echo "do you want to delete this setup file for the clirail script? [y/n]"
 read answer
 if  [[ $answer == "y" ]]; then
         rm test.bash
