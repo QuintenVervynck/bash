48edc2e3990293fec443398349bfdd652782d808
QuintenVervynck <quinten.vervynck@gmail.com>
2020-10-27 10:49:53 +0100

    made --open flag the default

--- chrome/chrome.bash
+++ chrome/chrome.bash
@@ -1,25 +1,22 @@
 #!/bin/bash
+if [[ $# -eq 0 ]]; then
+	google-chrome
+	exit 0
+fi
 
 case $1 in
 	-h | --help)
-		echo "usage : weather [options] [arguments]"
+		echo "usage : chrome [options] [arguments]"
 		echo "options	long		argument(s)	description"
-		echo "-o	--open		keyword		opens the url for the given keyword"
+		echo "			keyword		opens the url for the given keyword"
 		echo "-a	--add		keyword url	add a new keyword that refers to that url"
 		echo "-r	--remove	keyword		removes the keyword+url"
-		;;
-	-o| --open)
-		if [[ $# -eq 2 ]]; then
-			google-chrome -o $(cat ~/.data/chrome/$2)
-		else
-			echo "the -o (--open) flag requires 1 argument, the keyword"
-		fi
 		;;
 	-a| --add)
 		if [[ $# -eq 3 ]]; then
 			echo $3 > ~/.data/chrome/$2
 		else
-			echo "the -a (--add) flag requires 2 arguments, the keyword and the corresponding url"
+			echo "the -a (--add) flag requires 2 additional arguments, the keyword and the corresponding url"
 			exit 1
 		fi
 		;;
@@ -27,11 +24,11 @@ case $1 in
 		if [[ $# -eq 2 ]]; then
 			rm ~/.data/chrome/$2
 		else
-			echo "the -r (--remove) flag requires 1 argument, the keyword"
+			echo "the -r (--remove) flag requires an additional argument, the keyword"
 		fi
 		;;
 	*)
-		google-chrome
+		google-chrome -o $(cat ~/.data/chrome/$1)
 		;;
 esac
 
